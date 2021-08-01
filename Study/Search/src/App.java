75dbb9acd21279ef639591bf628782fa80ffd5be
jinhyeok15 <ekfrhd97@gmail.com>
2021-08-01 16:53:06 +0900

    자바 알고리즘

---
+++ Study/Search/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int[] a = {1,9,2,9,4,6,7,9};
+        int n = 8;
+        int key = 9;
+        int[] idx = new int[3];
+        int search = Q3.searchIdx(a, n, key, idx);
+        System.out.println(search);
+    }
+}
---
+++ Study/Search/src/Q3.java
@@ -0,0 +1,15 @@
+public class Q3 {
+    public static int searchIdx(int[] a, int n, int key, int[] idx) {
+        int nm=0;
+        for (int i=0; i<n; i++) {
+            if(a[i]==key) {
+                idx[nm] = i;
+                System.out.println(i);
+                if(i!=n-1){
+                    nm++;
+                }
+            }
+        }
+        return nm+1;
+    }
+}
