5ceb1d1247247ce72e0135f7581341e95bfc9841
jinhyeok15 <ekfrhd97@gmail.com>
2021-09-09 22:59:18 +0900

    bubbleSort ~Q4

--- Study/Algorithm/src/sort/BubbleSort.java
+++ Study/Algorithm/src/sort/BubbleSort.java
@@ -52,10 +52,102 @@ public class BubbleSort {
                     swap(a, j+1, j);
     }
 
+    static void bubbleSort3(int[] a, int n) {
+        int compare = 0;
+        int exchange = 0;
+        for (int i=0; i<n-1; i++) {
+            System.out.println("패스"+(i+1)+":");
+            // pass
+            System.out.print(" ");
+            int exchg = 0;
+            for (int j=n-1; j>i; j--){
+                System.out.print(" ");
+                for (int k=0; k<n; k++) {
+                    if(k==j-1){
+                        if(a[j-1] > a[j])
+                            System.out.print(a[k]+" + ");
+                        else
+                            System.out.print(a[k]+" - ");
+                    }
+                    else {
+                        System.out.print(a[k]+"   ");
+                    }
+                }
+
+                if (a[j-1]>a[j]){
+                    swap(a, j-1, j);
+                    exchg++;
+                    exchange++;
+                }
+                compare++;
+                System.out.println();
+            }
+            System.out.print(" ");
+            for (int j=0; j<n; j++) {
+                System.out.print(a[j]+"   ");
+            }
+            System.out.println();
+            if (exchg == 0) break;
+        }
+        System.out.println("비교를 "+compare+"회 "+"했습니다.");
+        System.out.println("교환을 "+exchange+"회 "+"했습니다.");
+    }
+
+    static void bubbleSort4(int[] a, int n) {
+        int compare = 0;
+        int exchange = 0;
+        int k = 0;
+        int i = 0;
+        while (k<n-1) {
+            System.out.println("패스"+(i+1)+":");
+            int last = n-1;
+            for(int j=n-1; j>k; j--) {
+                System.out.print(" ");
+                for (int o=0; o<n; o++) {
+                    if(o==j-1){
+                        if(a[j-1] > a[j])
+                            System.out.print(a[o]+" + ");
+                        else
+                            System.out.print(a[o]+" - ");
+                    }
+                    else {
+                        System.out.print(a[o]+"   ");
+                    }
+                }
+                if (a[j - 1] > a[j]) {
+                    swap(a, j - 1, j);
+                    last = j;
+                    exchange++;
+                }
+                compare++;
+                System.out.println();
+            }
+            k=last;
+            i++;
+            System.out.print(" ");
+            for (int j=0; j<n; j++) {
+                System.out.print(a[j]+"   ");
+            }
+            System.out.println();
+        }
+        System.out.println("비교를 "+compare+"회 "+"했습니다.");
+        System.out.println("교환을 "+exchange+"회 "+"했습니다.");
+    }
+
+    static void bubbleSort5(int[] a, int n) {
+        for (int i = n-1; i > 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (a[j + 1] < a[j]) {
+                    swap(a, j + 1, j);
+                }
+            }
+        }
+    }
+
     public static void main(String[] args) {
         Scanner stdIn = new Scanner(System.in);
 
-        System.out.println("버블 정렬(버전 1)");
+        System.out.println("버블 정렬(버전 2)");
         System.out.print("요솟수: ");
         int nx = stdIn.nextInt();
         int[] x = new int[nx];
@@ -65,7 +157,7 @@ public class BubbleSort {
             x[i] = stdIn.nextInt();
         }
 
-        bubbleSort(x, nx);
+        bubbleSort4(x, nx);
 
         System.out.println("오름차순으로 정렬했습니다.");
         for (int i = 0; i < nx; i++)
